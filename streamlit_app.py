7f5e682f55c39de5ecd46ef7f48f80e36d07affb
Jennifer Kercheval <118768073+jenkerch@users.noreply.github.com>
2022-11-28 15:31:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create function
-def get_fruitvice_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
      #streamlit.write('The user entered ', fruit_choice)
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     # remove raw file from screen - streamlit.text(fruityvice_response.json())
